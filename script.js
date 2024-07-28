6ca18e7ed993e1e61d639fef24b61a76973ac801
Wesley Torques <wesleyaugusto1145@gmail.com>
2024-07-28 18:59:08 -0300

    commit 2
    mudança de cores e size

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-var btn = document.querySelector(".no");
-var position;
-btn.addEventListener("click", function() {
-	position ? (position = 0) : (position = 150);
-	btn.style.transform = `translate(${position}px,0px)`;
-	btn.style.transition = "all 0.3s ease";
+let btn = document.querySelector(".no");
+let position = btn.addEventListener("click", function () {
+    position ? (position = 0) : (position = 150);
+    btn.style.transform = `translate(${position}px,0px)`;
+    btn.style.transition = "all 0.3s ease";
 });
 
-var btn = document.querySelector(".no");
-var position;
-btn.addEventListener("mouseover", function() {
-	position ? (position = 0) : (position = 400);
-	btn.style.transform = `translate(${position}px,0px)`;
-	btn.style.transition = "all 0.3s ease";
+btn = document.querySelector(".no");
+position = btn.addEventListener("mouseover", function () {
+    position ? (position = 0) : (position = 400);
+    btn.style.transform = `translate(${position}px,0px)`;
+    btn.style.transition = "all 0.3s ease";
 });
 
 function tanks() {
-	alert("Escolha sábia,Babyyy :)");
+    alert("Escolha sábia,Babyyy :)");
 }
